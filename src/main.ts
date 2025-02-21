443cfb6d0caf9c27ae5b42c7608be67a6b349dec
allydevper <alamaswilmer@gmail.com>
2025-02-20 23:19:45 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:4200', 'https://restaurant-management-production.up.railway.app'],
+  });
   await app.listen(3000);
 }
 bootstrap();
